ed84cab4dd1c7d8b7e4a696d3575994e041f94bb
Erick Briones <erickalan518@gmail.com>
2022-10-06 09:57:36 -0500

    Improving navigation

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,38 +1,40 @@
-import { FC } from "react"
-import { Grid, Card, Row, Text } from "@nextui-org/react"
+import { FC } from 'react';
+import { Grid, Card, Row, Text } from '@nextui-org/react';
+import NextLink from 'next/link';
 
-import { SmallPokemon } from "../../interfaces"
-import { useRouter } from "next/router"
+import { SmallPokemon } from '../../interfaces';
+import { useRouter } from 'next/router';
 
 interface Props {
-	pokemon: SmallPokemon
+	pokemon: SmallPokemon;
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
-
-	const router = useRouter()
+	const router = useRouter();
 
 	const handleOnClick = () => {
-		router.push(`/name/${pokemon.name}`)
-	} 
+		router.push(`/name/${pokemon.name}`);
+	};
 
 	return (
 		<Grid xs={6} sm={3} md={2} key={pokemon.id}>
-			<Card 
-				isHoverable 
-				isPressable
-				onPress={handleOnClick}
-			>
-				<Card.Body css={{ p: 1 }}>
-					<Card.Image src={pokemon.img} width="100%" />
-				</Card.Body>
-				<Card.Footer>
-					<Row justify="space-between">
-						<Text transform="capitalize">{pokemon.name}</Text>
-						<Text>#{pokemon.id}</Text>
-					</Row>
-				</Card.Footer>
-			</Card>
+			<NextLink href={`/name/${pokemon.name}`}>
+				<Card
+					isHoverable
+					isPressable
+					// onPress={handleOnClick}
+				>
+					<Card.Body css={{ p: 1 }}>
+						<Card.Image src={pokemon.img} width="100%" />
+					</Card.Body>
+					<Card.Footer>
+						<Row justify="space-between">
+							<Text transform="capitalize">{pokemon.name}</Text>
+							<Text>#{pokemon.id}</Text>
+						</Row>
+					</Card.Footer>
+				</Card>
+			</NextLink>
 		</Grid>
-	)
-}
+	);
+};
